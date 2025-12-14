6a33bc8080acd52de66aa3ee197336a3ba63ad35
David Briscoe <idbrii@gmail.com>
2025-12-14 10:45:02 -0800

    Example of code-driven Dialogs
    For popochiu/issues/260
    
    Demonstrate converting a dialog from inspector-driven to code-driven.
    This also converts _option_selected to use callback functions, but the
    unchanged _option_selected would also work the same way.
    
    The Popsy dialogue, is not quite the same. OfferHelp now starts disabled
    to demonstrate visibility changes work.
    
    :: Implement code-driven Dialogs ::
    
    Allow adding all options and strings for a Dialog from code. That allows
    you to see your whole conversation all in one place instead of flipping
    back and forth between code and inspector.
    
    You still need to create each Dialog in the Popochiu Inspector.
    
    In theory, you could use both kinds of options, but that seems too
    confusing to support.

--- game/dialogs/chat_with_popsy/dialog_chat_with_popsy.gd
+++ game/dialogs/chat_with_popsy/dialog_chat_with_popsy.gd
@@ -2,7 +2,8 @@
 extends PopochiuDialog
 
 
-#region Virtual ####################################################################################
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ Virtual ░░░░ {{{1
+
 func _on_start() -> void:
 	# One can put here something to excecute before showing the dialog options.
 	# E.g. Make the PC to look at the character which it will talk to, walk to
@@ -13,34 +14,49 @@ func _on_start() -> void:
 	await E.get_tree().process_frame
 
 
-func _option_selected(opt: PopochiuDialogOption) -> void:
-	# You can make the player character say the selected option with:
-#	await D.say_selected()
-	
-	# Use match to check which option was selected and excecute something for
-	# each one
-	match opt.id:
-		"StopWhining":
-			await C.Popsy.say("Bwaaah! I'm hungry!!!", "sad")
-			await C.player.say("I know, but please, stop crying like that...")
-			await C.Popsy.say("* sniff *")
-			opt.turn_off()
-			turn_on_options(["OfferHelp"])
-		"OfferHelp":
-			await D.say_selected()
-			await E.camera.shake_bg(1.0, 3.0)
-			await C.Popsy.say("I WANT AN APPLE!!!", "sad")
-		"PickToy":
-			await D.say_selected()
-			await C.Popsy.say("No, it's boring!")
-			await C.Popsy.say("I'm bored!")
-			await C.Popsy.say("AND I'M HUNGRY!!!", "sad")
-			await C.player.say("Oh my...")
-		"Bye":
-			await C.player.say("Hold on!")
-			stop()
-	
-	_show_options()
+func _build_options():
+	return [
+		create_option("StopWhining")
+			.with_text("Can you please stop whining for some minutes?")
+			,
+
+		create_option("OfferHelp")
+			.with_text("What can I do for you?")
+			.with_visible(false)
+			,
+
+		create_option("PickToy")
+			.with_text("Can you please pick up your toys?")
+			,
+
+		create_option("Bye")
+			.with_text("Going get you some food, hold on.")
+			,
+	]
+
+func _on_option_StopWhining(opt: PopochiuDialogOption):
+	await C.Popsy.say("Bwaaah! I'm hungry!!!", "sad")
+	await C.player.say("I know, but please, stop crying like that...")
+	await C.Popsy.say("* sniff *")
+	opt.turn_off()
+	turn_on_options(["OfferHelp"])
+
+func _on_option_OfferHelp(_opt: PopochiuDialogOption):
+	await D.say_selected()
+	await E.camera.shake_bg(1.0, 3.0)
+	await C.Popsy.say("I WANT AN APPLE!!!", "sad")
+
+func _on_option_PickToy(_opt: PopochiuDialogOption):
+	await D.say_selected()
+	await C.Popsy.say("No, it's boring!")
+	await C.Popsy.say("I'm bored!")
+	await C.Popsy.say("AND I'M HUNGRY!!!", "sad")
+	await C.player.say("Oh my...")
+
+func _on_option_Bye(_opt: PopochiuDialogOption):
+	await C.player.say("Hold on!")
+	stop()
+
 
 
 # Use this to save custom data for this PopochiuDialog when saving the game.
